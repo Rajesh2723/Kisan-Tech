51c1ebf562fcee3784c677a91de177cdd1c593d7
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2023-08-01 16:57:34 +0530

    Create DButils.java

---
+++ JavaTrainCodes/Nishi Assignments/DButils.java
@@ -0,0 +1,7 @@
+package Dbutils;
+
+public class DButils {
+	public final static String URL="jdbc:mysql://localhost:3306/compan";
+	public final static String USER="root";
+	public final static String PWD="1234";
+}
